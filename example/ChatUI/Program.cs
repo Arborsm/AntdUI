37e5f50e4fba00a5d54e27947848b3f40043b80f
Tom <17379620@qq.com>
2024-05-22 14:50:32 +0800

    ✉️ 新增 Overview 新增AOT 编译、ChatUI 聊天示例

---
+++ example/ChatUI/Program.cs
@@ -0,0 +1,17 @@
+namespace ChatUI
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.Run(new Main());
+        }
+    }
+}
