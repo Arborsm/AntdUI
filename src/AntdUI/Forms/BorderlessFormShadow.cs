754b4c5b87f6f209fd17214bca269212ae10b8d0
Tom <17379620@qq.com>
2024-07-12 11:39:00 +0800

    👾 BorderlessForm 新增 ShadowPierce、修复 Spin 位置不移动、Drawer 新增 加载关闭回调、开放 Spin

--- src/AntdUI/Forms/BorderlessFormShadow.cs
+++ src/AntdUI/Forms/BorderlessFormShadow.cs
@@ -55,6 +55,7 @@ namespace AntdUI
             {
                 var cp = base.CreateParams;
                 cp.ExStyle |= 0x08000000 | 0x00080000;
+                if (form != null && form.ShadowPierce) cp.ExStyle |= 0x20;
                 cp.Parent = IntPtr.Zero;
                 return cp;
             }
